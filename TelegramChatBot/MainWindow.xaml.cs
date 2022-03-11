5c57414a3f5e8155858723476834a391e08f9d3b
DimaSavrasov45 <Ginder19@i.ua>
2022-03-11 20:14:14 +0200

    TelegramBot

---
+++ TelegramChatBot/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telegram.Bot;
+using System.Collections.ObjectModel;
+using Telegram.Bot.Args;
+using System.IO;
+using System.Diagnostics;
+
+namespace TelegramChatBot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<TelegramSender> Senders;
+        TelegramBotClient bot;
+        private static string token = "5296648309:AAHKcVrc80cVmUxlTekQdpa2vfvlNUqJi5w";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Senders = new ObservableCollection<TelegramSender>();
+            ListSender.ItemsSource = Senders;
+            bot = new TelegramBotClient(token);
+
+
+            bot.OnMessage += delegate (object sender, MessageEventArgs e)
+            {
+
+                string msg = $"{DateTime.Now} : {e.Message.Chat.FirstName} {e.Message.Chat.Id}   {e.Message.Text}";
+                File.AppendAllText("data.log", $"{msg}\n");
+
+                Debug.WriteLine(msg);
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    TelegramSender person = new TelegramSender(e.Message.Chat.FirstName, e.Message.Chat.Id);
+                    if (!Senders.Contains(person)) Senders.Add(person);
+                    Senders[Senders.IndexOf(person)].AddMessage($"{person.Nick} : {e.Message.Text}");
+                });
+
+
+
+
+            };
+            bot.StartReceiving();
+
+            btnSendMessage.Click += delegate { SendMessage(); }; 
+
+
+
+
+         }
+        public void SendMessage()
+        {
+            var concreateSender = Senders[Senders.IndexOf(ListSender.SelectedItem as TelegramSender)];
+            string responseMsg = $"Support : {textOfAnswer.Text}";
+            concreateSender.Messages.Add(responseMsg);
+
+            bot.SendTextMessageAsync(concreateSender.Id, textOfAnswer.Text);
+            string logText = $"{DateTime.Now} >> {concreateSender.Id} {concreateSender.Nick} {responseMsg}\n";
+            File.AppendAllText("data.log", logText);
+            textOfAnswer.Text = String.Empty;
+        }
+
+     
+    }
+
+
+}
+
+
+
---
+++ TelegramChatBot/TelegramSender.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace TelegramChatBot
+{
+    public class TelegramSender : IEquatable<TelegramSender>, INotifyPropertyChanged
+    {
+
+        public TelegramSender(string Nickname, long ChatId)
+        {
+            this.id = ChatId;
+            this.nick = Nickname;
+            Messages = new ObservableCollection<string>();
+        }
+
+        private string nick;
+        public string Nick
+        {
+            get { return this.nick; }
+
+            set
+            {
+                this.nick = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Nick)));
+
+            }
+        }
+        private long id;
+        public long Id
+        {
+            get { return this.id; }
+            set
+            {
+                this.id = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Id)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool Equals(TelegramSender other) => other.Id == this.id;
+        public ObservableCollection<string> Messages { get; set; }
+        public void AddMessage(string Text) => Messages.Add(Text);
+
+
+
+
+    }
+}
